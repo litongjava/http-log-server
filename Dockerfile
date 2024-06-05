48e48f8acf3b54880685eb4041fd1b58774f4986
litongjava <litongjava@qq.com>
2024-06-05 06:43:01 -1000

    init commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Use the offical Golang image to build the app: https://hub.docker.com/_/golang
+# FROM golang:1.22.1 as builder
+FROM litongjava/hertz:0.9.0-golang1.22.1 as builder
+
+# Copy code to the image
+WORKDIR /
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+# Build the app
+RUN CGO_ENABLED=0 GOOS=linux go build -v -o /app.out main.go
+
+# Start a new image for production without build dependencies
+FROM litongjava/alpine:base
+# RUN apk add --no-cache ca-certificates
+# RUN apk add --no-cache tzdata
+
+# Copy the app binary from the builder to the production image
+COPY --from=builder /app.out /app
+
+# Run the app when the vm starts
+CMD ["/app"]
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "context"
+  "github.com/cloudwego/hertz/pkg/app"
+  "github.com/cloudwego/hertz/pkg/app/server"
+  "github.com/cloudwego/hertz/pkg/common/hlog"
+  "github.com/cloudwego/hertz/pkg/common/utils"
+  "io"
+  "log"
+  "os"
+  "strings"
+)
+
+func init() {
+  f, err := os.OpenFile("/data/app.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  fileWriter := io.MultiWriter(f, os.Stdout)
+  log.SetOutput(fileWriter)
+  log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+}
+func main() {
+  h := server.Default()
+  h.POST("/log", PostLogHandler)
+  h.Spin()
+}
+
+func PostLogHandler(ctx context.Context, reqCtx *app.RequestContext) {
+  var reqVo struct {
+    Level string   `json:"level"`
+    Args  []string `json:"args"`
+  }
+  token := os.Getenv("token")
+  authorization := string(reqCtx.GetHeader("Authorization"))
+
+  if !strings.HasSuffix(authorization, token) {
+    reqCtx.JSON(403, utils.H{"error": "Unauthorized"})
+    return
+  }
+
+  err := reqCtx.BindJSON(&reqVo)
+  if err != nil {
+    hlog.Error(err)
+    reqCtx.JSON(400, utils.H{"error": "Invalid request payload"})
+    return
+  }
+  go func() {
+    log.Printf("%s %v", reqVo.Level, reqVo.Args)
+  }()
+}
